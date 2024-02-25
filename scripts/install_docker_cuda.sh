93a0d73d6c9a55c2026180a92ccb420aa40c0218
Matt Nowak <17482065+Tuee22@users.noreply.github.com>
2024-02-25 17:11:29 -0500

    initial commit

--- scripts/install_docker_cuda.sh
+++ scripts/install_docker_cuda.sh
@@ -47,7 +47,7 @@ sudo apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force
 echo "Installing Docker..."
 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
